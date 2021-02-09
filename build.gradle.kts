332703580f8883ee247a87378603be8d59d0123c
AdoniasAlcantara <adoniasasneto@gmail.com>
2021-02-09 07:58:17 -0300

    build: add Jsoup

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jsoup:jsoup:1.13.1")
+
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
