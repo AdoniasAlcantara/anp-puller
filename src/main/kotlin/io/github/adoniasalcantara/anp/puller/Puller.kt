35a7c47614826745295ad9a8962ecfa2cd38f553
AdoniasAlcantara <adoniasasneto@gmail.com>
2021-02-11 22:23:24 -0300

    feat: add Puller

---
+++ src/main/kotlin/io/github/adoniasalcantara/anp/puller/Puller.kt
@@ -0,0 +1,25 @@
+package io.github.adoniasalcantara.anp.puller
+
+import io.github.adoniasalcantara.anp.model.City
+import io.github.adoniasalcantara.anp.model.FuelType
+import io.github.adoniasalcantara.anp.model.Station
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+class Puller(
+    private val target: String,
+    private val cookie: Pair<String, String>,
+    private val weekCode: Int
+) {
+    fun fetch(city: City, fuelType: FuelType): Document {
+        return Jsoup.connect(target)
+            .ignoreContentType(true)
+            .cookie(cookie.first, cookie.second)
+            .data("COD_SEMANA", "$weekCode")
+            .data("COD_MUNICIPIO", "${city.code}")
+            .data("DESC_MUNICIPIO", city.name)
+            .data("COD_COMBUSTIVEL", "${fuelType.code}")
+            .data("DESC_COMBUSTIVEL", fuelType.name)
+            .post()
+    }
+}
