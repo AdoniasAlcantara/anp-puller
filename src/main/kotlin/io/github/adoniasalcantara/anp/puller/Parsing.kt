85b573c0855fdb09746c7ed7f22811c9901f0282
AdoniasAlcantara <adoniasasneto@gmail.com>
2021-02-11 22:51:46 -0300

    feat: add functions for parsing

---
+++ src/main/kotlin/io/github/adoniasalcantara/anp/puller/Parsing.kt
@@ -0,0 +1,83 @@
+package io.github.adoniasalcantara.anp.puller
+
+import io.github.adoniasalcantara.anp.model.Fuel
+import io.github.adoniasalcantara.anp.model.FuelType
+import io.github.adoniasalcantara.anp.model.Station
+import org.jsoup.nodes.Document
+import java.math.BigDecimal
+import java.security.MessageDigest
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+private val dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+private val sanitizeRegex = Regex("[<>!:;+-]")
+
+fun makeSha1Key(input: String): String {
+    return MessageDigest
+        .getInstance("SHA-1")
+        .digest(input.toByteArray())
+        .fold("") { str, byte -> str + "%02x".format(byte) }
+}
+
+fun sanitize(input: String): String {
+    return input
+        .replace(sanitizeRegex, "")
+        .removeSuffix("/")
+}
+
+fun parseDate(dateInput: String): LocalDate {
+    return LocalDate.parse(sanitize(dateInput), dateFormatter)
+}
+
+fun parseFuelType(fuelTypeInput: String): FuelType {
+    FuelType.values().forEach {
+        if (fuelTypeInput.endsWith(it.name)) return it
+    }
+
+    error("There is no fuel type associated with \"$fuelTypeInput\"")
+}
+
+fun parseCurrency(currencyInput: String): BigDecimal {
+    return currencyInput
+        .replace(",", ".")
+        .replace("-", "")
+        .toBigDecimal()
+}
+
+fun parseDocument(document: Document): List<Station> {
+    val city = document.selectFirst("table > tbody > tr > td > b")
+        .text()
+        .split("-")[1]
+
+    val fuelType = document.selectFirst("body > table > tbody > tr:nth-child(2) > td > b")
+        .text()
+        .run(::parseFuelType)
+
+    val rows = document.selectFirst("table.table_padrao > tbody")
+        .children()
+        .drop(2) // Remove headers
+
+    return rows.map { row ->
+        val data = row.children()
+        val company = data[0].text()
+        val brand = data[3].text()
+        val address = data[1].text()
+        val neighborhood = data[2].text()
+        val fuel = Fuel(
+            fuelType,
+            parseDate(data[5].text()),
+            parseCurrency(data[4].text())
+        )
+
+        return@map Station(
+            makeSha1Key("$company$address$neighborhood$city$brand"),
+            sanitize(company),
+            sanitize(address),
+            sanitize(neighborhood),
+            sanitize(city),
+            sanitize(brand),
+            listOf(fuel)
+        )
+    }
+}
