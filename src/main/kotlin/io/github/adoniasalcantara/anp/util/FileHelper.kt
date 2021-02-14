344ae3544f1d781bedff31a3409e704c5295dfdb
AdoniasAlcantara <adoniasasneto@gmail.com>
2021-02-13 21:14:58 -0300

    feat: add FileHelper prototype

---
+++ src/main/kotlin/io/github/adoniasalcantara/anp/util/FileHelper.kt
@@ -0,0 +1,19 @@
+package io.github.adoniasalcantara.anp.util
+
+import io.github.adoniasalcantara.anp.model.City
+import io.github.adoniasalcantara.anp.model.Station
+
+class FileHelper {
+
+    fun readCities(): List<City> {
+        TODO("Not implemented")
+    }
+
+    fun writeTemp(taskId: Int, stations: List<Station>) {
+        TODO("Not implemented")
+    }
+
+    fun writeConcat() {
+        TODO("Not implemented")
+    }
+}
