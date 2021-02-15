bf275bb0a73fc988a9d6a293c9cb9295ae30741f
AdoniasAlcantara <adoniasasneto@gmail.com>
2021-02-15 08:48:31 -0300

    feat: read cities from JSON file

--- src/main/kotlin/io/github/adoniasalcantara/anp/util/FileHelper.kt
+++ src/main/kotlin/io/github/adoniasalcantara/anp/util/FileHelper.kt
@@ -2,11 +2,20 @@ package io.github.adoniasalcantara.anp.util
 
 import io.github.adoniasalcantara.anp.model.City
 import io.github.adoniasalcantara.anp.model.Station
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import java.nio.file.Files
+import java.nio.file.Path
 
-class FileHelper {
+class FileHelper(
+    private val tempDir: Path,
+    private val citiesFile: Path,
+    private val concatFile: Path
+) {
 
     fun readCities(): List<City> {
-        TODO("Not implemented")
+        val str = Files.readString(citiesFile)
+        return Json.decodeFromString(str)
     }
 
     fun writeTemp(taskId: Int, stations: List<Station>) {
