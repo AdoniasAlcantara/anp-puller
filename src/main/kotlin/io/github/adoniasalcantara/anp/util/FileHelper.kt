1308b06f1adc547410558735b95d7d7549f64b09
AdoniasAlcantara <adoniasasneto@gmail.com>
2021-02-15 16:57:10 -0300

    feat: write temp file

--- src/main/kotlin/io/github/adoniasalcantara/anp/util/FileHelper.kt
+++ src/main/kotlin/io/github/adoniasalcantara/anp/util/FileHelper.kt
@@ -3,6 +3,7 @@ package io.github.adoniasalcantara.anp.util
 import io.github.adoniasalcantara.anp.model.City
 import io.github.adoniasalcantara.anp.model.Station
 import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.nio.file.Files
 import java.nio.file.Path
@@ -12,6 +13,10 @@ class FileHelper(
     private val citiesFile: Path,
     private val concatFile: Path
 ) {
+    init {
+        val temp = tempDir.toFile()
+        if (!temp.exists()) temp.mkdir()
+    }
 
     fun readCities(): List<City> {
         val str = Files.readString(citiesFile)
@@ -19,7 +24,9 @@ class FileHelper(
     }
 
     fun writeTemp(taskId: Int, stations: List<Station>) {
-        TODO("Not implemented")
+        val str = Json.encodeToString(stations)
+        val tempFile = tempDir.resolve("$taskId.temp.json")
+        Files.writeString(tempFile, str)
     }
 
     fun writeConcat() {
