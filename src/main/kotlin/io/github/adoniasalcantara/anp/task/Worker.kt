dc17a9eaa4f4571be999ac20ab068fede93309c2
AdoniasAlcantara <adoniasasneto@gmail.com>
2021-02-20 12:05:53 -0300

    refactor: decouple Worker from the queuing logic

--- src/main/kotlin/io/github/adoniasalcantara/anp/task/Worker.kt
+++ src/main/kotlin/io/github/adoniasalcantara/anp/task/Worker.kt
@@ -1,47 +1,41 @@
 package io.github.adoniasalcantara.anp.task
 
-import io.github.adoniasalcantara.anp.util.FileHelper
 import io.github.adoniasalcantara.anp.model.FuelType
 import io.github.adoniasalcantara.anp.model.Station
 import io.github.adoniasalcantara.anp.puller.Puller
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.Default
+import io.github.adoniasalcantara.anp.util.FileHandler
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.withContext
 
 class Worker(
     private val puller: Puller,
-    private val fileHelper: FileHelper
+    private val fileHandler: FileHandler
 ) {
-    fun runAsync(supply: () -> Task?) = CoroutineScope(Default).async {
-        while (true) {
-            // Worker gets as many tasks as it is able to process.
-            // If there are no more tasks in the queue, worker
-            // leaves the loop and finishes the job.
-            val (taskId, city) = supply() ?: break
+    suspend fun run(task: Task) = coroutineScope {
+        val (taskId, city) = task
 
-            // Fire jobs concurrently for each fuel type
-            val jobs = FuelType.values().map { fuelType ->
-                async(IO) { puller.fetch(city, fuelType) }
+        // Fire requests concurrently for each fuel type
+        val results = FuelType.values().map { fuelType ->
+            async(IO) { puller.fetch(city, fuelType) }
+        }
+
+        // Merge partial results into a single map
+        val combinedResult = mutableMapOf<String, Station>()
+
+        results.awaitAll().flatten().forEach { station ->
+            combinedResult.merge(station.key, station) { old, new ->
+                old.copy(fuels = old.fuels + new.fuels)
             }
+        }
 
-            // Merge all partial results into a single map
-            val combinedResult = mutableMapOf<String, Station>()
+        val stations = combinedResult.values.toList()
 
-            jobs.awaitAll().flatten().forEach { station ->
-                combinedResult.merge(station.key, station) { old, new ->
-                    old.copy(fuels = old.fuels + new.fuels)
-                }
-            }
-
-            val stations = combinedResult.values.toList()
-
-            // Write the combined result to a temp file
-            withContext(IO) {
-                fileHelper.writeTemp(taskId, stations)
-            }
+        // Write combined result to a temp file
+        withContext(IO) {
+            fileHandler.writeTemp(taskId, stations)
         }
     }
 }
