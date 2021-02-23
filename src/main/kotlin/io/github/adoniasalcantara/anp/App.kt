861c450a7bf7dfa5bd49911a4ca466734730c416
AdoniasAlcantara <adoniasasneto@gmail.com>
2021-02-22 22:14:41 -0300

    feat: initialize app components

--- src/main/kotlin/io/github/adoniasalcantara/anp/App.kt
+++ src/main/kotlin/io/github/adoniasalcantara/anp/App.kt
@@ -1,5 +1,36 @@
 package io.github.adoniasalcantara.anp
 
-fun main() {
-    println("Hello world!")
+import io.github.adoniasalcantara.anp.puller.Puller
+import io.github.adoniasalcantara.anp.task.Task
+import io.github.adoniasalcantara.anp.task.TaskRunner
+import io.github.adoniasalcantara.anp.task.Worker
+import io.github.adoniasalcantara.anp.util.FileHandler
+import kotlinx.coroutines.runBlocking
+import java.nio.file.Paths
+
+fun main(vararg args: String) = runBlocking {
+    val config = getConfig(
+        Paths.get(args.getOrElse(0) { "./config.json" })
+    )
+
+    val fileHandler = config.run {
+        FileHandler(
+            Paths.get(tempDir),
+            Paths.get(citiesFile),
+            Paths.get(outFile)
+        )
+    }
+
+    val puller = config.run {
+        Puller(
+            targetUrl,
+            cookieKey to cookieValue,
+            weekCode
+        )
+    }
+
+    val tasks = fileHandler.readCities().mapIndexed(::Task)
+    val workers = List(config.numWorkers) { Worker(puller, fileHandler) }
+    TaskRunner(tasks, workers).run()
+    fileHandler.writeConcat()
 }
